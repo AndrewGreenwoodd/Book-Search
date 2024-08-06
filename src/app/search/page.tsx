cbe1ae96598c2658fcdbd8f61c3d884116631b5f
Andrii Sasnyk <andriy258022@gmail.com>
2024-08-06 14:37:22 +0300

    outsourced fetching logic

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,59 +1,20 @@
 "use client";
 
+import React, { useEffect } from "react";
 import { useSearchParams } from "next/navigation";
-import axios from "axios";
-import { useEffect, useState, useCallback } from "react";
-import Card from "@/components/Card";
-import { Book } from "../types/Book.types";
 import Link from "next/link";
 import { useInView } from "react-intersection-observer";
+import useBookSearch from "@/hooks/useBookSearch";
+import Card from "@/components/Card";
 
 const SearchResults: React.FC = () => {
   const searchParams = useSearchParams();
-  const query = searchParams.get("query");
-  const [bookData, setBookData] = useState<Book[]>([]);
-  const [page, setPage] = useState(0);
-  const [loading, setLoading] = useState(false);
-  const [hasMore, setHasMore] = useState(true);
-  const apiKey = process.env.NEXT_PUBLIC_GOOGLE_API;
-
+  const query = searchParams.get("query") || "";
+  const { bookData, fetchBooks, loading, hasMore } = useBookSearch(query);
   const { ref: loadMoreRef, inView } = useInView({
     threshold: 1,
     triggerOnce: false,
   });
-
-  const filterDuplicates = (books: Book[], newBooks: Book[]): Book[] => {
-    //sometimes Books API sends duplicate books
-    const existingIds = new Set(books.map((book) => book.id));
-    return newBooks.filter((book) => !existingIds.has(book.id));
-  };
-
-  const fetchBooks = useCallback(async () => {
-    setLoading(true);
-    try {
-      const response = await axios.get(
-        `https://www.googleapis.com/books/v1/volumes?q=${query}&key=${apiKey}&startIndex=${
-          page * 20
-        }&maxResults=20`
-      );
-      const newBooks = response.data.items || [];
-      if (newBooks.length < 20) {
-        setHasMore(false);
-      }
-      const uniqueBooks = filterDuplicates(bookData, newBooks);
-      setBookData((prevData) => [...prevData, ...uniqueBooks]);
-      setPage((prevPage) => prevPage + 1);
-    } catch (error: any) {
-      if (error.response?.status === 429) {
-        console.log("Rate limit exceeded, retrying in 2 seconds...");
-        setTimeout(() => fetchBooks(), 2000); // If you scroll to fast, Books API will throw an error, so we wait 2 seconds to limit requests
-      } else {
-        console.error(error);
-      }
-    } finally {
-      setLoading(false);
-    }
-  }, [query, apiKey, page, bookData]);
 
   useEffect(() => {
     if (inView && !loading && hasMore) {
---
+++ src/hooks/useBookSearch.tsx
@@ -0,0 +1,49 @@
+import { useState, useEffect, useCallback } from "react";
+import axios from "axios";
+import { Book } from "@/app/types/Book.types";
+
+const useBookSearch = (query: string) => {
+  const [bookData, setBookData] = useState<Book[]>([]);
+  const [page, setPage] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [hasMore, setHasMore] = useState(true);
+  const apiKey = process.env.NEXT_PUBLIC_GOOGLE_API;
+
+  const filterDuplicates = (books: Book[], newBooks: Book[]): Book[] => {
+    //sometimes Books API sends duplicate books
+    const existingIds = new Set(books.map((book) => book.id));
+    return newBooks.filter((book) => !existingIds.has(book.id));
+  };
+
+  const fetchBooks = useCallback(async () => {
+    setLoading(true);
+    try {
+      const response = await axios.get(
+        `https://www.googleapis.com/books/v1/volumes?q=${query}&key=${apiKey}&startIndex=${
+          page * 20
+        }&maxResults=20`
+      );
+      const newBooks = response.data.items || [];
+      if (newBooks.length < 20) {
+        setHasMore(false);
+      }
+      const uniqueBooks = filterDuplicates(bookData, newBooks);
+      setBookData((prevData) => [...prevData, ...uniqueBooks]);
+      setPage((prevPage) => prevPage + 1);
+    } catch (error: any) {
+      if (error.response?.status === 429) {
+        console.log("Rate limit exceeded, retrying in 2 seconds...");
+        // If you scroll to fast, Books API will throw an error, so we wait 2 seconds to limit requests
+        setTimeout(() => fetchBooks(), 2000);
+      } else {
+        console.error(error);
+      }
+    } finally {
+      setLoading(false);
+    }
+  }, [query, apiKey, page, bookData]);
+
+  return { bookData, fetchBooks, loading, hasMore, setPage };
+};
+
+export default useBookSearch;
